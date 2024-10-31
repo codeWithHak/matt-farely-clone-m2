e95971732d52d45ad7e48a344bda04a0dd09b102
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-10-31 21:26:44 +0500

    fixed apos error in work

--- src/app/components/about/About.tsx
+++ src/app/components/about/About.tsx
@@ -5,7 +5,7 @@ function About() {
     <div className='about-bg'>
         <div className='about-content'>
             <h1>Hi, I&apos;m Matt. Nice to meet you.</h1>
-            <p>Since beginning my journey as a freelance designer 12 years ago, I've done remote work for agencies, consulted for startups, and collaborated with talented people to create digital products for both business and consumer use. I'm quietly confident, naturally curious, and perpetually working on improving my chops.</p>
+            <p>Since beginning my journey as a freelance designer 12 years ago, I&apos;ve done remote work for agencies, consulted for startups, and collaborated with talented people to create digital products for both business and consumer use. I&apos;m quietly confident, naturally curious, and perpetually working on improving my chops.</p>
         </div>
     </div>
   )
